ec49c0308ec144768434e924c00036956107d0bf
KnuckleB0ll <singhsaksham2409@gmail.com>
2024-06-29 13:14:14 +0530

    git

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model.js";
+import bcrypyt from "bcryptjs";
 
 export const signup = async (req, res) => {
     try {
@@ -14,7 +15,8 @@ export const signup = async (req, res) => {
         }
 
         // HASH PASSWORD
-        // Add your password hashing logic here
+        const salt = await bcrypyt.genSalt(10);
+        const hashedPassword = await bcrypyt.hash(password, salt);
 
         const boyProfilePic = `https://avatar-placeholder.iran.liara.run/public/boy?username=${username}`;
         const girlProfilePic = `https://avatar-placeholder.iran.liara.run/public/girl?username=${username}`;
@@ -22,19 +24,25 @@ export const signup = async (req, res) => {
         const newUser = new User({
             fullName,
             username,
-            password,
+            password: hashedPassword,
             gender,
             profilePic: gender === "male" ? boyProfilePic : girlProfilePic
         })
 
-        await newUser.save();
+        if(newUser){
+            //generate jwt token
+            await newUser.save();
 
-        res.status(201).json({  //201 means created successfully
-            _id: newUser._id,
-            fullName: newUser.fullName,
-            username: newUser.username,
-            profilePic: newUser.profilePic
-        });
+            res.status(201).json({  //201 means created successfully
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                username: newUser.username,
+                profilePic: newUser.profilePic
+            });
+        }
+        else{
+            res.status(400).json({ error: "Invalid User Data" })
+        }
         
     } catch (error) {
         console.log("Error in signup controller:", error);

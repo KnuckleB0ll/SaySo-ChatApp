f82d8c5e13bae7342f3186994b1149049b7de150
KnuckleB0ll <singhsaksham2409@gmail.com>
2024-08-08 02:54:52 +0530

    initiate deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -11,6 +12,8 @@ import { app, server } from "./socket/socket.js";
 
 const PORT = process.env.port || 5000;
 
+const __dirname = path.resolve();
+
 dotenv.config();
 
 app.use(express.json()); // This is a middleware that allows us to parse incoming requests with JSON payloads
@@ -19,12 +22,16 @@ app.use(cookieParser()); // This is a middleware that allows us to parse incomin
 app.use("/api/auth",authRoutes);                                                                        
 app.use("/api/messages",messageRoutes);                                                                        
 app.use("/api/users",userRoutes);                                                                        
-// app.use("/api/users",userRoutes);
 
 
-// app.get("/", (req, res) => {
-//     res.send("Hello World");
-// });
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
+
+
 
 
 
